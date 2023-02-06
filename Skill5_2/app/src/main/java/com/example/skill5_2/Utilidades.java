58b2990aa965b8a1e6dbccff5088576f0796a66f
ismaelblanquez <ismaelblanquez@gmail.com>
2023-02-06 23:06:51 +0100

    06/02/23

---
+++ Skill5_2/app/src/main/java/com/example/skill5_2/Utilidades.java
@@ -0,0 +1,10 @@
+package com.example.skill5_2;
+
+public class Utilidades {
+
+    public static final int LIST=1;
+    public static final int GRID=2;
+
+    public static int visualizacion=LIST;
+
+}
